c1195292efa529ab1c593b4dcd5fbf3033abd33b
Nutchy <nutchy.th@gmail.com>
2020-08-16 18:48:34 +0700

    Hide unused section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Y18Hero />
-    <Hero />
+    <!-- <Hero />
     <WhatIsYWC />
     <Majors />
     <Location />
@@ -12,31 +12,31 @@
     <QA />
     <FollowUs />
     <Footer />
-    <PR />
+    <PR />-->
   </div>
 </template>
 
 <script>
 import Y18Hero from '~/components/sections/ywc18/Hero.vue'
-import WhatIsYWC from '~/components/sections/WhatIsYWC'
-import Hero from '~/components/sections/Hero'
-import Timeline from '~/components/sections/Timeline.vue'
+// import WhatIsYWC from '~/components/sections/WhatIsYWC'
+// import Hero from '~/components/sections/Hero'
+// import Timeline from '~/components/sections/Timeline.vue'
 export default {
   components: {
-    Y18Hero,
-    Hero,
-    WhatIsYWC,
-    Location: () => import('~/components/sections/Location'),
-    Timeline,
-    Guru: () => import('~/components/sections/Guru'),
-    Majors: () => import('~/components/sections/Majors'),
-    QA: () => import('~/components/sections/QA'),
-    FollowUs: () => import('~/components/sections/FollowUs'),
-    Sponsor: () => import('~/components/sections/Sponsor'),
-    Gallery: () => import('~/components/sections/Gallery'),
-    PR: () => import('~/components/sections/PR'),
-    Footer: () => import('~/components/sections/Footer'),
-  },
+    Y18Hero
+    // Hero,
+    // WhatIsYWC,
+    // Location: () => import('~/components/sections/Location'),
+    // Timeline,
+    // Guru: () => import('~/components/sections/Guru'),
+    // Majors: () => import('~/components/sections/Majors'),
+    // QA: () => import('~/components/sections/QA'),
+    // FollowUs: () => import('~/components/sections/FollowUs'),
+    // Sponsor: () => import('~/components/sections/Sponsor'),
+    // Gallery: () => import('~/components/sections/Gallery'),
+    // PR: () => import('~/components/sections/PR'),
+    // Footer: () => import('~/components/sections/Footer'),
+  }
 }
 </script>
 
