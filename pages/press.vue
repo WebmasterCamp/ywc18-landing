7dce4661e7e1a42b2ac4dabee6c8ff9f3d7b1934
Chayanon Thongpila <nutchy.th@gmail.com>
2020-10-18 19:05:29 +0700

    Update press image size

--- pages/press.vue
+++ pages/press.vue
@@ -1,84 +1,123 @@
 <template>
   <div>
     <ThemeProvider :name="currTheme">
-    <SectionTitle title="ร่วมประชาสัมพันธ์" />
-    <section class="container" style="margin-bottom: 60px;">
-      <h3>เนื้อหาประชาสัมพันธ์</h3>
-      <div ref="pr-content" class="pr-content">
-        <h4 class="right">ข่าวประชาสัมพันธ์</h4>
-        <h4 class="center">
-          Young Webmaster Camp ครั้งที่ 18 โอกาสครั้งใหญ่กับมิติใหม่ที่คุณจะได้ระเบิดศักยภาพในสายงานด้านดิจิทัล
-        </h4>
-        <p>
-          กลับมาอีกครั้งกับค่ายที่จะมอบความรู้และประสบการณ์เจาะลึกหลากแง่มุมทางด้านเว็บไซต์และวงการดิจิทัลอย่างเต็มรูปแบบ <b>“Young Webmaster Camp ครั้งที่ 18 : New Normal, New Web, New World”</b> ค่ายสร้างเว็บไซต์ที่ยิ่งใหญ่ที่สุดในประเทศ เชิญชวนเหล่านิสิตนักศึกษา นักเรียนหรือบุคคลทั่วไป อายุระหว่าง 18 - 23 ปี ที่มีใจรักในการสร้างสรรค์เว็บไซต์ มาร่วมกันระเบิดศักยภาพ แสดงพลังไอเดียของเหล่าคนรุ่นใหม่ ที่พร้อมจะสร้างโลกใหม่ที่ดียิ่งขึ้น
-        </p>
-        <p>
-          ซึ่งกิจกรรมภายในค่ายได้เปิดโอกาสให้ผู้ร่วมกิจกรรม ได้ระดมไอเดียในการสร้างเว็บไซต์ที่สามารถใช้งานได้จริง พร้อมทั้งรับความรู้และประสบการณ์ในการทำงานจริง จากเหล่ากูรูผู้เชี่ยวชาญในวงการ ไม่ว่าจะเป็น พี่เอ็ม ขจร จาก Mango Zero, พี่ตั้ง จาก Wisesight, พี่โน้ต จาก Dek-D.com, พี่บอย จาก Pantip.com และอีกมากมาย โดยในวันสุดท้าย ผู้เข้าร่วมกิจกรรมจะได้นำเสนอผลงานต่อคณะกรรมการ พร้อมรับคำแนะนำเพื่อนำไปพัฒนาและฝึกฝนต่อในอนาคต
-        </p>
-        <p>
-          ผู้เข้าร่วมกิจกรรมสามารถเลือกสมัครสาขาตามความสนใจหรือตามความถนัด ซึ่งประกอบไปด้วยทั้งหมด 4 สาขา คือ
-        </p>
-        <ol>
-          <li>
-            สาขาเว็บคอนเทนต์ (Web Content) นักเล่าเรื่องตัวยงที่ก้าวทันสถานการณ์ พร้อมเรียบเรียงข้อมูลด้วยความสดใหม่ ใส่ใจประเด็น และมุ่งเน้นที่จะสร้างสรรค์สิ่งใหม่ให้ดีกว่าเดิม
-          </li>
-          <li>
-            สาขาเว็บดีไซน์ (Web Design) ใจรักการสร้างภาพ ทำงานศิลป์ เล่นสีตอบโจทย์อารมณ์และความต้องการของผู้เข้าเว็บไซต์ ตลอดจนดึงดูดใจด้วยสไตล์การออกแบบ
-          </li>
-          <li>
-            สาขาเว็บมาร์เก็ตติ้ง (Web Marketing) เผด็จศึกทุกปัญหาด้วยการวางกลยุทธ์ พร้อมปรับตัวและพัฒนาขีดจำกัดเพื่อการเติบโตของธุรกิจในทุกสภาวะอย่างมั่นคงแข็งแรง
-          </li>
-          <li>
-            สาขาเว็บโปรแกรมมิ่ง (Web Programming) เก่งในการจัดการความคิด รักการพัฒนา กล้าทดลองและสามารถเสกโค้ดสุดคูลเพื่อนำมาตอบสนองวิถีใหม่ในไลฟ์สไตล์ New Normal
-          </li>
-        </ol>
-        <p>
-          ซึ่งในปีนี้ค่ายได้จัดขึ้นที่ SCB Academy ตั้งแต่วันที่ 24 - 27 ธันวาคม 2563 กิจกรรมเป็น รูปแบบไป-กลับ ถึงแม้ว่าในปีนี้จะไม่มีการค้างคืน แต่กิจกรรมภายในก็จัดมาแบบเน้น ๆ แบบเต็มอิ่ม ทั้งความรู้จากวิทยากร สถานที่ กิจกรรมและอาหารเหมือนเดิม และแน่นอนว่า “ไม่มีค่าใช้จ่ายในการสมัครและการเข้าร่วมกิจกรรมภายในค่าย” มาแค่ตัวกับใจ ได้เพื่อนและความรู้กลับไปแน่นอน!
-        </p>
-        <p>
-          สำหรับใครที่สนใจสมัครค่าย Young Webmaster Camp ครั้งที่ 18 สามารถสมัครได้ตั้งแต่วันนี้ - 7 พฤศจิกายน 2563 ที่ www.ywc.in.th
-        </p>
-        <p>
-          สามารถติดตามข่าวสารเพิ่มเติมได้ที่ <br>
-          Facebook : Young Webmaster Camp<br>
-          Instagram : @ywcth<br>
-          Twitter : @ywcth
-        </p>
-      </div>
-      <BannerButton
-        color="orange"
-        style="margin:0 auto;margin-top:10px;"
-        @click="selectText($event, 'pr-content')"
-      >
-        คัดลอกทั้งหมด
-      </BannerButton>
-    </section>
-    <section class="container" style="margin-bottom: 120px;">
-      <h3 style="margin-bottom:50px">ภาพแบนเนอร์</h3>
-      <div v-for="(isShow, size) in bannerList" :key="size">
-        <BannerItem>
-          <BannerCol>
-            <img v-lazy="getSrc(size)" />
-          </BannerCol>
-          <BannerCol>
-            ขนาด {{ size }}px
-            <BannerButton color="orange" :href="getSrc(size)" target="_blank" rel="noopener noreferrer">
-              ดาวน์โหลด
-            </BannerButton>
-            <BannerButton color="orange" @click="viewCode(size)">
-              {{ isShow ? 'ซ่อน' : 'แสดง' }} Code
-            </BannerButton>
-          </BannerCol>
-        </BannerItem>
-        <textarea
-          v-show="isShow"
-          @mouseover="selectText"
-          v-html="getBannerCode(getSrc(size, true))"
-        ></textarea>
-      </div>
-    </section>
-    <SponsorBox/>
-    <Footer />
+      <SectionTitle title="ร่วมประชาสัมพันธ์" />
+      <section class="container" style="margin-bottom: 60px;">
+        <h3>เนื้อหาประชาสัมพันธ์</h3>
+        <div ref="pr-content" class="pr-content">
+          <h4 class="right">ข่าวประชาสัมพันธ์</h4>
+          <h4 class="center">
+            Young Webmaster Camp ครั้งที่ 18
+            โอกาสครั้งใหญ่กับมิติใหม่ที่คุณจะได้ระเบิดศักยภาพในสายงานด้านดิจิทัล
+          </h4>
+          <p>
+            กลับมาอีกครั้งกับค่ายที่จะมอบความรู้และประสบการณ์เจาะลึกหลากแง่มุมทางด้านเว็บไซต์และวงการดิจิทัลอย่างเต็มรูปแบบ
+            <b
+              >“Young Webmaster Camp ครั้งที่ 18 : New Normal, New Web, New
+              World”</b
+            >
+            ค่ายสร้างเว็บไซต์ที่ยิ่งใหญ่ที่สุดในประเทศ เชิญชวนเหล่านิสิตนักศึกษา
+            นักเรียนหรือบุคคลทั่วไป อายุระหว่าง 18 - 23 ปี
+            ที่มีใจรักในการสร้างสรรค์เว็บไซต์ มาร่วมกันระเบิดศักยภาพ
+            แสดงพลังไอเดียของเหล่าคนรุ่นใหม่ ที่พร้อมจะสร้างโลกใหม่ที่ดียิ่งขึ้น
+          </p>
+          <p>
+            ซึ่งกิจกรรมภายในค่ายได้เปิดโอกาสให้ผู้ร่วมกิจกรรม
+            ได้ระดมไอเดียในการสร้างเว็บไซต์ที่สามารถใช้งานได้จริง
+            พร้อมทั้งรับความรู้และประสบการณ์ในการทำงานจริง
+            จากเหล่ากูรูผู้เชี่ยวชาญในวงการ ไม่ว่าจะเป็น พี่เอ็ม ขจร จาก Mango
+            Zero, พี่ตั้ง จาก Wisesight, พี่โน้ต จาก Dek-D.com, พี่บอย จาก
+            Pantip.com และอีกมากมาย โดยในวันสุดท้าย
+            ผู้เข้าร่วมกิจกรรมจะได้นำเสนอผลงานต่อคณะกรรมการ
+            พร้อมรับคำแนะนำเพื่อนำไปพัฒนาและฝึกฝนต่อในอนาคต
+          </p>
+          <p>
+            ผู้เข้าร่วมกิจกรรมสามารถเลือกสมัครสาขาตามความสนใจหรือตามความถนัด
+            ซึ่งประกอบไปด้วยทั้งหมด 4 สาขา คือ
+          </p>
+          <ol>
+            <li>
+              สาขาเว็บคอนเทนต์ (Web Content)
+              นักเล่าเรื่องตัวยงที่ก้าวทันสถานการณ์
+              พร้อมเรียบเรียงข้อมูลด้วยความสดใหม่ ใส่ใจประเด็น
+              และมุ่งเน้นที่จะสร้างสรรค์สิ่งใหม่ให้ดีกว่าเดิม
+            </li>
+            <li>
+              สาขาเว็บดีไซน์ (Web Design) ใจรักการสร้างภาพ ทำงานศิลป์
+              เล่นสีตอบโจทย์อารมณ์และความต้องการของผู้เข้าเว็บไซต์
+              ตลอดจนดึงดูดใจด้วยสไตล์การออกแบบ
+            </li>
+            <li>
+              สาขาเว็บมาร์เก็ตติ้ง (Web Marketing)
+              เผด็จศึกทุกปัญหาด้วยการวางกลยุทธ์
+              พร้อมปรับตัวและพัฒนาขีดจำกัดเพื่อการเติบโตของธุรกิจในทุกสภาวะอย่างมั่นคงแข็งแรง
+            </li>
+            <li>
+              สาขาเว็บโปรแกรมมิ่ง (Web Programming) เก่งในการจัดการความคิด
+              รักการพัฒนา
+              กล้าทดลองและสามารถเสกโค้ดสุดคูลเพื่อนำมาตอบสนองวิถีใหม่ในไลฟ์สไตล์
+              New Normal
+            </li>
+          </ol>
+          <p>
+            ซึ่งในปีนี้ค่ายได้จัดขึ้นที่ SCB Academy ตั้งแต่วันที่ 24 - 27
+            ธันวาคม 2563 กิจกรรมเป็น รูปแบบไป-กลับ
+            ถึงแม้ว่าในปีนี้จะไม่มีการค้างคืน แต่กิจกรรมภายในก็จัดมาแบบเน้น ๆ
+            แบบเต็มอิ่ม ทั้งความรู้จากวิทยากร สถานที่ กิจกรรมและอาหารเหมือนเดิม
+            และแน่นอนว่า
+            “ไม่มีค่าใช้จ่ายในการสมัครและการเข้าร่วมกิจกรรมภายในค่าย”
+            มาแค่ตัวกับใจ ได้เพื่อนและความรู้กลับไปแน่นอน!
+          </p>
+          <p>
+            สำหรับใครที่สนใจสมัครค่าย Young Webmaster Camp ครั้งที่ 18
+            สามารถสมัครได้ตั้งแต่วันนี้ - 7 พฤศจิกายน 2563 ที่ www.ywc.in.th
+          </p>
+          <p>
+            สามารถติดตามข่าวสารเพิ่มเติมได้ที่ <br />
+            Facebook : Young Webmaster Camp<br />
+            Instagram : @ywcth<br />
+            Twitter : @ywcth
+          </p>
+        </div>
+        <BannerButton
+          color="orange"
+          style="margin:0 auto;margin-top:10px;"
+          @click="selectText($event, 'pr-content')"
+        >
+          คัดลอกทั้งหมด
+        </BannerButton>
+      </section>
+      <section class="container" style="margin-bottom: 120px;">
+        <h3 style="margin-bottom:50px">ภาพแบนเนอร์</h3>
+        <div v-for="(isShow, size) in bannerList" :key="size">
+          <BannerItem>
+            <BannerCol>
+              <img v-lazy="getSrc(size)" />
+            </BannerCol>
+            <BannerCol>
+              ขนาด {{ size }}px
+              <BannerButton
+                color="orange"
+                :href="getSrc(size)"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ดาวน์โหลด
+              </BannerButton>
+              <BannerButton color="orange" @click="viewCode(size)">
+                {{ isShow ? 'ซ่อน' : 'แสดง' }} Code
+              </BannerButton>
+            </BannerCol>
+          </BannerItem>
+          <textarea
+            v-show="isShow"
+            @mouseover="selectText"
+            v-html="getBannerCode(getSrc(size, true))"
+          ></textarea>
+        </div>
+      </section>
+      <SponsorBox />
+      <Footer />
     </ThemeProvider>
   </div>
 </template>
@@ -99,6 +138,7 @@ const BannerItem = styled('div')`
   }
 `
 const BannerCol = styled('div')`
+  font-family: Anuphan, system-ui, -apple-system, sans-sreif;
   display: inline-grid;
   align-content: start;
   text-align: left;
@@ -170,15 +210,15 @@ export default {
     ThemeProvider,
     SectionTitle: () => import('~/components/ywc18/SectionHead.vue'),
     Footer: () => import('~/components/sections/ywc18/Footer'),
-    SponsorBox: () => import('~/components/SponsorBox')
+    SponsorBox: () => import('~/components/SponsorBox'),
   },
   data() {
     return {
       bannerList: {
         // '160x600': false,
         // '300x50': false,
-        // '300x250': false,
-        // '300x600': false,
+        '300x250': false,
+        '300x600': false,
         // '320x50': false,
         '320x100': false,
         '728x90': false,
